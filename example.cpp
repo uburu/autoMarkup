cc7f0f20d6b9008ee5c8884f40ba28a93595384a
nikitarub <nikita.rub@yandex.ru>
2018-05-02 14:24:17 +0300

    #4 Updated example and added build&run

--- example.cpp
+++ example.cpp
@@ -10,20 +10,45 @@ int main(int argc, const char * argv[]) {
     
     DataHub dh;
     dh.ReadText(file);
+    
     file.close();
 
     std::cout << dh.getText() << std::endl;
 
 
     file.open("./data/dict.txt");
-    
-    DictHandler dict;
-    dict.fillDict(file, " ", ",");
+    dh.ReadDict(file, " ", ",");
     file.close();
+
+    // set an example tokens
+    std::vector<token_t> tokens_tmp;
+    token_t tmp_words_1, tmp_words_2;
+    tmp_words_1.push_back("привет");
+	tmp_words_1.push_back("как");
+	tmp_words_1.push_back("дела");
+	tokens_tmp.push_back(tmp_words_1);
+	
+	tmp_words_2.push_back("хорошо");
+	tmp_words_2.push_back("а");
+	tmp_words_2.push_back("у");
+	tmp_words_2.push_back("тебя");
+	tokens_tmp.push_back(tmp_words_2);
+
+
+    dh.setTokens(tokens_tmp);
+
+    std::cout << "size of sentences of tokens in DataHub: " << dh.getTokens().size() << std::endl;
+
+    DictHandler dict_table;
+    dict_table.fitModel(dh);
+    
     
 
-    std::cout << dict.getId("привет") << std::endl;
-    
+    std::cout << "id of word 'привет': " << dict_table.getId("привет") << std::endl;
+
+    std::cout << "\nword '" << dh.getDict()[2].word << "' has id [2] or 3 in dict.txt " << std::endl;
+    std::cout << "1-st word: '" << dh.getTokens()[1][0] << "' from 2-nd sentence has id: " 
+    << dh.getSents_id()[1].word_id[0] << std::endl;
     
     return 0;
 }

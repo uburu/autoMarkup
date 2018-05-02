3e189b7ff64a2d33bcf577e56276d15b81f77454
nikitarub <nikita.rub@yandex.ru>
2018-05-02 14:20:45 +0300

    #1 Added get, set and add methods to DataHub
    get, set and add methods to work with DataHub’s attributes

--- autoMarkup/DataHub/DataHub.cpp
+++ autoMarkup/DataHub/DataHub.cpp
@@ -52,12 +52,33 @@ void DataHub::ReadDict(std::ifstream& file, std::string delimiter, std::string d
 };
 
 
-std::string DataHub::getText()
-{
-	return text;
-}
-
-std::vector<dictelem_t> DataHub::getDict()
-{
-	return dict;
-}
+// void DataHub::ReadTokens(std::ifstream& input, std::string delimiter_words, std::string delimiter_lines)
+// {
+//     std::string s; // содержит строку файла
+    
+//     while (std::getline(file, s)) // получение единой строки из текста
+//     {
+        
+//         unsigned long id = 0;
+//         std::string word;
+//         std::vector<double> token_line;
+        
+//         int i = 0;
+//         size_t pos = 0;
+//         std::string token;
+//         while ((pos = s.find(delimiter)) != std::string::npos) {
+//             token = s.substr(0, pos);
+//             if (i == 0)
+//                 id = atoi(token.c_str());
+//             else if (i == 1)
+//                 word = token;
+            
+//             s.erase(0, pos + delimiter.length());
+//             i++;
+//         }
+        
+        
+//         dictelem_t elem(id, word, vec);
+//         dict.push_back(elem);
+//     }
+// }

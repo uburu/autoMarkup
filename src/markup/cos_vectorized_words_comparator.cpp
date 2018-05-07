05f74848e29cf6cf086875cb4e4778a0cdddba54
denis96z <zinoviev.denis@yandex.ru>
2018-05-06 22:58:07 -0700

    Cos comparator implemented

--- src/markup/cos_vectorized_words_comparator.cpp
+++ src/markup/cos_vectorized_words_comparator.cpp
@@ -1,7 +1,21 @@
+#include <cassert>
+#include <cmath>
+
 #include <common/types.hpp>
 #include <markup/cos_vectorized_words_comparator.hpp>
 
 double markup::CosVectorizedWordsComparator::ApplyTo(const common::vectorized_word_t &left,
                                                      const common::vectorized_word_t &right) {
-    throw common::NotImplementedException("markup::CosVectorizedWordsComparator::ApplyTo");
+    assert(left.size() == right.size());
+
+    double comp = 0.0;
+    double sqrLeft = 0.0, sqrRight = 0.0;
+
+    for (size_t i = 0; i < left.size(); ++i) {
+        comp += left[i] * right[i];
+        sqrLeft += left[i] * left[i];
+        sqrRight += right[i] * right[i];
+    }
+
+    return comp / (sqrt(sqrLeft) * sqrt(sqrRight));
 }

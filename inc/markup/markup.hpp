96ec5eecce76ab25eeb86b8c3d5f6b2901685673
denis96z <zinoviev.denis@yandex.ru>
2018-05-07 00:00:06 -0700

    Refactoring.

--- inc/markup/markup.hpp
+++ inc/markup/markup.hpp
@@ -6,44 +6,44 @@
 namespace markup
 {
     class Markup {
-    public:
-        Markup() = default;
-        Markup(const Markup &markup) = default;
-        Markup(Markup &&markup) noexcept = default;
+        public:
+            Markup() = default;
+            Markup(const Markup &markup) = default;
+            Markup(Markup &&markup) noexcept = default;
 
-        virtual ~Markup() noexcept = default;
+            virtual ~Markup() noexcept = default;
 
-        Markup& operator =(const Markup &markup) = default;
-        Markup& operator =(Markup &&markup) noexcept = default;
+            Markup& operator =(const Markup &markup) = default;
+            Markup& operator =(Markup &&markup) noexcept = default;
 
-        /**
-         * Определяет смысловую близость двух векторизованных предложений.
-         * @param firstSent
-         * Первое предложение в векторизованном виде.
-         * @param secondSent
-         * Второе предложение в векторизованном виде.
-         * @return
-         * Коэффициент близости двух векторов: 0 <= k <= 1.
-         */
-        virtual double MarkupSentences(const common::vectorized_sentence_t &firstSent,
-                                       const common::vectorized_sentence_t &secondSent);
+            /**
+             * Определяет смысловую близость двух векторизованных предложений.
+             * @param firstSent
+             * Первое предложение в векторизованном виде.
+             * @param secondSent
+             * Второе предложение в векторизованном виде.
+             * @return
+             * Коэффициент близости двух векторов: 0 <= k <= 1.
+             */
+            virtual double MarkupSentences(const common::vectorized_sentence_t &firstSent,
+                                           const common::vectorized_sentence_t &secondSent);
 
-        /**
-         * Определяет смысловую близость двух векторизованных текстов.
-         * @param firstText
-         * Первый текст в векторизованном виде.
-         * @param secondText
-         * Второй текст в векторизованном виде.
-         * @param firstWindowSize
-         * Размер "окна" в первом тексте.
-         * @param secondWindowSize
-         * Размер "окна" во втором тексте.
-         * @return
-         * Коэффициент близости двух текстов: 0 <= k <= 1.
-         */
-        virtual double MarkupTexts(const common::vectorized_text_t &firstText,
-                                   const common::vectorized_text_t &secondText,
-                                   size_t firstWindowSize, size_t secondWindowSize) = 0;
+            /**
+             * Определяет смысловую близость двух векторизованных текстов.
+             * @param firstText
+             * Первый текст в векторизованном виде.
+             * @param secondText
+             * Второй текст в векторизованном виде.
+             * @param firstWindowSize
+             * Размер "окна" в первом тексте.
+             * @param secondWindowSize
+             * Размер "окна" во втором тексте.
+             * @return
+             * Коэффициент близости двух текстов: 0 <= k <= 1.
+             */
+            virtual double MarkupTexts(const common::vectorized_text_t &firstText,
+                                       const common::vectorized_text_t &secondText,
+                                       size_t firstWindowSize, size_t secondWindowSize) = 0;
     };
 }
 

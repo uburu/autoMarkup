c67d1f76826f7ab6d0ef7ed2e947f620a96453b6
denis96z <zinoviev.denis@yandex.ru>
2018-05-08 01:58:06 -0700

    Some markup tests added - not passed: memory access exception.

--- inc/markup/concurrent_markup.hpp
+++ inc/markup/concurrent_markup.hpp
@@ -1,6 +1,7 @@
 #ifndef MARKUP_CONCURRENT_MARKUP_HPP
 #define MARKUP_CONCURRENT_MARKUP_HPP
 
+#include <future>
 #include "markup.hpp"
 
 namespace markup {
@@ -37,13 +38,44 @@ namespace markup {
             double MarkupTexts(const common::vectorized_text_t &firstText,
                                const common::vectorized_text_t &secondText,
                                size_t firstWindowSize, size_t secondWindowSize) override;
+
+        private:
+            common::vectorized_sentence_t FindTextConvolution(const common::vectorized_text_t &text);
     };
 
     template<typename C, typename CV>
     double ConcurrentMarkup<C, CV>::MarkupTexts(const common::vectorized_text_t &firstText,
                                                 const common::vectorized_text_t &secondText,
                                                 size_t firstWindowSize, size_t secondWindowSize) {
-        return 0;
+        assert(!firstText.empty() && !secondText.empty());
+
+        auto firstConv = std::move(FindTextConvolution(firstText));
+        auto secondConv = std::move(FindTextConvolution(secondText));
+
+        double simCoeff = 0.0;
+        for (size_t index = 0; index < std::min(firstText.size(), secondText.size()); ++index) {
+            simCoeff = std::max(simCoeff, this->wordsComparator.ApplyTo(firstConv[index], secondConv[index]));
+        }
+
+        return simCoeff;
+    }
+
+    template<typename C, typename CV>
+    common::vectorized_sentence_t ConcurrentMarkup<C, CV>
+            ::FindTextConvolution(const common::vectorized_text_t &text) {
+        std::vector<std::future<common::vectorized_word_t>> fut(text.size());
+        for (size_t i = 0; i < text.size(); ++i) {
+            fut[i] = std::async(std::launch::async, [&]() {
+                return Markup<C, CV>::sentenceConvolution.ApplyTo(text[i]);
+            });
+        }
+
+        common::vectorized_sentence_t conv(text.size());
+        for (size_t i = 0; i < text.size(); ++i) {
+            conv[i] = std::move(fut[i].get());
+        }
+
+        return conv;
     }
 }
 
--- test/markup/test_markup.hpp
+++ test/markup/test_markup.hpp
@@ -4,16 +4,71 @@
 #include <boost/test/unit_test.hpp>
 #include "../fakeit/fakeit.hpp"
 
+#include <random>
+
 #include <markup/markup.hpp>
+#include <markup/sequential_markup.hpp>
+#include <markup/concurrent_markup.hpp>
+
 #include <markup/cos_words_comparator.hpp>
 #include <markup/sentence_sum_convolution.hpp>
 
-BOOST_AUTO_TEST_SUITE(TestMarkupSentences)
+common::vectorized_word_t create_word();
+common::vectorized_sentence_t create_sentence();
+common::vectorized_text_t create_text();
 
-    BOOST_AUTO_TEST_CASE(Correct) {
-        throw common::NotImplementedException("TestMarkupSentences");
+BOOST_AUTO_TEST_SUITE(TestMarkupTexts)
+
+    BOOST_AUTO_TEST_CASE(ValidCoeff) {
+        auto t1 = create_text();
+        auto t2 = create_text();
+
+        typedef markup::CosWordsComparator C;
+        typedef markup::SentenceSumConvolution CV;
+
+        markup::ConcurrentMarkup<C, CV> concMarkup;
+        auto concCoeff = concMarkup.MarkupTexts(t1, t2, 10, 2);
+        BOOST_CHECK(concCoeff >= 0.0 && concCoeff <= 1.0);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(TestMarkupSentences)
+
+    BOOST_AUTO_TEST_CASE(NoExceptions) {
+        typedef markup::CosWordsComparator C;
+        typedef markup::SentenceSumConvolution CV;
+        fakeit::Mock<markup::Markup<C, CV>> markup;
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+common::vectorized_word_t create_word() {
+    const size_t wSize = 5;
+
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(1.0, 10.0);
+
+    return common::vectorized_word_t(wSize, dist(mt));
+}
+
+common::vectorized_sentence_t create_sentence() {
+    auto size = static_cast<size_t>((rand() % 10) + 1);
+    common::vectorized_sentence_t s(size);
+    for (size_t i = 0; i < size; ++i) {
+        s[i] = create_word();
+    }
+    return s;
+}
+
+common::vectorized_text_t create_text() {
+    auto size = static_cast<size_t>((rand() % 10) + 1);
+    common::vectorized_text_t t(size);
+    for (size_t i = 0; i < size; ++i) {
+        t[i] = create_sentence();
+    }
+    return t;
+}
 
 #endif //TEST_MARKUP_MARKUP_HPP
